5d51d8c2959e1f85a9e8991cbc33c16b263293c5
최진석 <79374648+choiapple@users.noreply.github.com>
2023-10-21 20:48:53 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    if(s == "NLCS"){
+        cout<<"North London Collegiate School"<<"\n";
+    }else if(s == "BHA"){
+        cout<<"Branksome Hall Asia"<<"\n";
+    }else if(s == "KIS"){
+        cout<<"Korea International School"<<"\n";
+    }else if(s == "SJA"){
+        cout<<"St. Johnsbury Academy"<<"\n";
+    }
+}
