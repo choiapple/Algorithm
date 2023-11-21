16980e4d3cb7365f6d050473ecd5adaa1cace06d
최진석 <79374648+choiapple@users.noreply.github.com>
2023-11-21 09:23:11 +0900

    [Silver V] Title: 이장님 초대, Time: 48 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool cmp(int a, int b){
+    return a > b;
+}
+int main(){
+    int N;
+    int arr[100001] = {0, };
+    cin>>N;
+    for(int n=0; n<N; n++){
+        cin>>arr[n];
+    }
+    sort(arr, arr+N, cmp);
+    int answer = 1;
+    int tmp = 1;
+    for(int i=0; i<N; i++){
+        if(answer < arr[i] + i + 1){
+            answer = arr[i] + i + 1;
+        }
+    }
+    cout<<answer+1<<"\n";
+}
