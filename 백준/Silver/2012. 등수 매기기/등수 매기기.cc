2fecb2c195674c60dd0a712ba9010bceeb1d031d
최진석 <79374648+choiapple@users.noreply.github.com>
2023-11-23 10:03:23 +0900

    [Silver III] Title: 등수 매기기, Time: 80 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.cc
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.cc
@@ -9,33 +9,13 @@ int main(){
     int N;
     cin>>N;
     int arr[500001] = {0, };
-    int visit[500001] = {0, };
-    int visit2[500001] = {0, };
     for(int i=0; i<N; i++){
         cin>>arr[i];
     }
     sort(arr, arr+N);
-    for(int i=0; i<N; i++){
-        int tmp = arr[i];
-        if(tmp <= N && visit2[i] == 0 && visit[tmp] == 0){
-            visit[tmp] = 1;
-            visit2[i] = 1;
-        }
-    }
-    vector<int> vc;
-    vector<int> vc2;
-    for(int i=0; i<N; i++){
-        if(visit2[i] == 0){
-            vc.push_back(arr[i]);
-        }
-        if(visit[i+1] == 0){
-            vc2.push_back(i+1);
-        }
-    }
     long long answer = 0;
-    int len = vc.size();
-    for(int i=0; i<len; i++){
-        answer += abs(vc[i] - vc2[i]);
+    for(int i=0; i<N; i++){
+        answer += abs(i + 1 - arr[i]);
     }
     cout<<answer<<"\n";
     
