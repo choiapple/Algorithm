da024f3c26b26097eaf8ba3e0481bc53e1dcc9d6
최진석 <79374648+choiapple@users.noreply.github.com>
2023-11-27 09:28:43 +0900

    [Silver V] Title: 칠무해, Time: 2844 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.cc
+++ 백준/Silver/14729. 칠무해/칠무해.cc
@@ -1,19 +1,30 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    int N;
+    double tmp;
+    cin>>N;
+    priority_queue<double> pq;
+    for(int i=0; i<N; i++){
+        cin>>tmp;
+        pq.push(tmp);
+        if(pq.size() > 7){
+            pq.pop();
+        }
+    }
+    double arr[8] = {0, };
+    for(int i=0; i<7; i++){
+        arr[i] = pq.top();
+        pq.pop();
+    }
+    sort(arr, arr+7);
     cout<<fixed;
     cout.precision(3);
-    int N;
-    cin>>N;
-    double arr[10000001] = {0, };
-    for(int i=0; i<N; i++){
-        cin>>arr[i];
-    }
-    sort(arr, arr+N);
     for(int i=0; i<7; i++){
         cout<<arr[i]<<"\n";
     }
