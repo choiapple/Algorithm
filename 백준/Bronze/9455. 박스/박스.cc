3465c93173690d91c3f237b26161000a1e1b39e6
최진석 <79374648+choiapple@users.noreply.github.com>
2023-12-25 12:58:32 +0900

    [Bronze I] Title: 박스, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9455. 박스/박스.cc
+++ 백준/Bronze/9455. 박스/박스.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int T, m, n;
     cin>>T;
     for(int i=0; i<T; i++){
