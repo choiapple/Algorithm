0632ee3049037528c5acd8dfc13c057ac327d4f9
최진석 <79374648+choiapple@users.noreply.github.com>
2023-12-25 12:55:58 +0900

    [Bronze I] Title: 박스, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int T, m, n;
+    cin>>T;
+    for(int i=0; i<T; i++){
+        cin>>m>>n;
+        int arr[101][101] = {0, };
+        int answer = 0;
+        for(int j=0; j<m; j++){
+            for(int k=0; k<n; k++){
+                cin>>arr[j][k];
+            }
+        }
+        for(int j=0; j<m; j++){
+            for(int k=0; k<n; k++){
+                if(arr[j][k] == 1){
+                    for(int x=j; x<m; x++){
+                        if(arr[x][k] == 0){
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+        cout<<answer<<"\n";
+    }
+}
