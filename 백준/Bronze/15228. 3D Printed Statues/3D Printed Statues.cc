3f7e20a105236a466cf34a232e574b74c6035541
최진석 <79374648+choiapple@users.noreply.github.com>
2023-12-27 13:01:20 +0900

    [Bronze I] Title: 3D Printed Statues, Time: 492 ms, Memory: 2072 KB -BaekjoonHub

---
+++ 백준/Bronze/15228. 3D Printed Statues/3D Printed Statues.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int N;
+int answer = 214700000;
+void solv(int prin, int dong, int tmp){
+    if(answer < tmp){
+        return;
+    }
+    if(prin > N){
+        return;
+    }
+    if(dong >= N){
+        if(answer > tmp){
+            answer = tmp;
+        }
+        return;
+    }
+    solv(prin, dong + prin, tmp+1);
+    solv(prin + prin, dong, tmp+1);
+    
+}
+int main(){
+    cin>>N;
+    solv(1, 0, 0);
+    cout<<answer<<"\n";
+}
