e0ab9aa7945c599a4465f6b2a72d843133a7d9e7
최진석 <79374648+choiapple@users.noreply.github.com>
2024-01-06 14:50:00 +0900

    [Bronze I] Title: 콘도 선정, Time: 100 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2246. 콘도 선정/콘도 선정.cc
+++ 백준/Bronze/2246. 콘도 선정/콘도 선정.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int N, D, C;
     int answer = 0;
     int arr[10001][2] = {0, };
