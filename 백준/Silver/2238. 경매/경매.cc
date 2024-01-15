006fa87c673a8b7196c007f16b5d5b7b3ab74804
최진석 <79374648+choiapple@users.noreply.github.com>
2024-01-16 07:47:13 +0900

    [Silver V] Title: 경매, Time: 20 ms, Memory: 5808 KB -BaekjoonHub

--- 백준/Silver/2238. 경매/경매.cc
+++ 백준/Silver/2238. 경매/경매.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int U, N;
     string s[100001];
     int arr[100001] = {0, };
