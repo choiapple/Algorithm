a4d60a4012c9f5c7c28b1bec86f91e29670cf5a7
최진석 <79374648+choiapple@users.noreply.github.com>
2024-01-16 07:46:30 +0900

    [Silver V] Title: 경매, Time: 84 ms, Memory: 5808 KB -BaekjoonHub

---
+++ 백준/Silver/2238. 경매/경매.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int U, N;
+    string s[100001];
+    int arr[100001] = {0, };
+    int visit[100001] = {0, };
+    cin>>U>>N;
+    for(int i=0; i<N; i++){
+        cin>>s[i]>>arr[i];
+        visit[arr[i]]++;
+    }
+    int min_n = 100001;
+    int tmp = 0;
+    for(int i=0; i<N; i++){
+        if(visit[arr[i]] < min_n){
+            min_n = visit[arr[i]];
+            tmp = arr[i];
+        }
+    }
+    for(int i=0; i<N; i++){
+        if(min_n == visit[arr[i]] && tmp > arr[i]){
+            tmp = arr[i];
+        }
+    }
+    for(int i=0; i<N; i++){
+        if(tmp == arr[i]){
+            cout<<s[i]<<" "<<arr[i]<<"\n";
+            return 0;
+        }
+    }
+}
