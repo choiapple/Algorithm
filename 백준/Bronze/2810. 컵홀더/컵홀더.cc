9147eed41ff27683e17edd8ffe5b4d917a5290a9
최진석 <79374648+choiapple@users.noreply.github.com>
2024-02-06 13:44:22 +0900

    [Bronze I] Title: 컵홀더, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2810. 컵홀더/컵홀더.cc
+++ 백준/Bronze/2810. 컵홀더/컵홀더.cc
@@ -7,9 +7,7 @@ int main(){
     cin>>s;
     int answer = 1;
     for(int i=0; i<N; i++){
-        if(i > 0 && s[i-1] != 'L' && s[i] == 'S'){
-            answer++;
-        }else if(s[i] == 'L'){
+       if(s[i] == 'L'){
             i++;
             answer++;
         }else if(s[i] == 'S'){
