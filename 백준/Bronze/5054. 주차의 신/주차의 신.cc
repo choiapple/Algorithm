39322c926ce448a0cdb60a9b5200cb816984ae53
최진석 <79374648+choiapple@users.noreply.github.com>
2024-04-09 09:52:01 +0900

    [Bronze II] Title: 주차의 신, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5054. 주차의 신/주차의 신.cc
+++ 백준/Bronze/5054. 주차의 신/주차의 신.cc
@@ -2,6 +2,9 @@
 #include <algorithm>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int t, n;
     cin>>t;
     for(int i=0; i<t; i++){
