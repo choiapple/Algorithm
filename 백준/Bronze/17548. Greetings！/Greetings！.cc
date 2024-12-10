3ac6938874c2a19fba0c456bbb851587ef7cda56
최진석 <79374648+choiapple@users.noreply.github.com>
2024-12-11 00:34:04 +0900

    [Bronze IV] Title: Greetings!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17548. Greetings！/Greetings！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string s;
+    int cnt = 0, len;
+    cin>>s;
+    len = s.length()-2;
+    cout<<"h";
+    for(int i=0; i<2*len; i++){
+        cout<<"e";
+    }
+    cout<<"y";
+}
