91cebeb4fcc6bbbf1d834bf2e2eeb5cc7fe68a7f
최진석 <79374648+choiapple@users.noreply.github.com>
2024-12-24 23:01:43 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=0; i<N*5; i++){
+        if(i < N || i >= N*5 - N){
+            for(int j=0; j<N*5; j++){
+                cout<<"@";
+            }
+            cout<<"\n";
+        }else{
+            for(int j=0; j<N; j++){
+                cout<<"@";
+            }
+            cout<<"\n";
+        }
+    }
+}
