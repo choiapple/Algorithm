16b8d5714a5932ba7e601b814ff25a5e0d4f5544
최진석 <79374648+choiapple@users.noreply.github.com>
2025-01-08 23:56:56 +0900

    [Bronze II] Title: ZOAC 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/18238. ZOAC 2/ZOAC 2.cc
+++ 백준/Bronze/18238. ZOAC 2/ZOAC 2.cc
@@ -1,41 +1,18 @@
 #include <iostream>
+#include <algorithm>
 #include <cmath>
 using namespace std;
 int main(){
     char cur;
     string s;
-    int len, answer = 0, left, right, tmp;
+    int len, answer = 0, left, right;
     cin>>s;
     len = s.length();
     cur = 'A';
     for(int i=0; i<len; i++){
-        left = 0;
-        for(int j=0; j<26; j++){
-            tmp = cur - 'A' - j;
-            if(tmp >= 0 && tmp == s[i] - 'A'){
-                left = j;
-                break;
-            }else if(tmp < 0 && tmp + 26 == s[i] - 'A'){
-                left = j;
-                break;
-            }
-        }
-        right = 0;
-        for(int j=0; j<26; j++){
-            tmp = cur - 'A' + j;
-            if(tmp < 26 && tmp == s[i] - 'A'){
-                right = j;
-                break;
-            }else if(tmp >= 26 && tmp - 26 == s[i] - 'A'){
-                right = j;
-                break;
-            }
-        }
-        if(left < right){
-            answer += left;
-        }else{
-            answer += right;
-        }
+        right = abs(s[i] - cur);
+        left = 26 - right;
+        answer += min(right, left);
         cur = s[i];
     }
     cout<<answer<<"\n";
