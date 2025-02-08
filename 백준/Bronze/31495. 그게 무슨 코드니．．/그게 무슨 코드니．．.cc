ab7c4a7be059630339cb4e5c7a5c94aad5249068
최진석 <79374648+choiapple@users.noreply.github.com>
2025-02-08 22:23:38 +0900

    [Bronze IV] Title: 그게 무슨 코드니.., Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.cc
+++ 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.cc
@@ -5,7 +5,7 @@ int main(){
     string s;
     getline(cin, s);
     int len = s.length();
-    if(s != "\"" && s != "\"\"" && s[0] == '\"' && s[len-1] == '\"'){
+    if(len > 2 && s[0] == '\"' && s[len-1] == '\"'){
         cout<<s.substr(1,len-2)<<"\n";
     }else{
         cout<<"CE"<<"\n";
