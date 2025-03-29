0050d986691ce720b7dc8be4119e7968bdeda825
최진석 <79374648+choiapple@users.noreply.github.com>
2025-03-29 16:24:52 +0900

    [Bronze II] Title: 순서쌍, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15701. 순서쌍/순서쌍.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    long long N, answer = 0;
+    cin>>N;
+    for(int i=1; i<=sqrt(N); i++){
+        if(N%i != 0) continue;
+        if(i != N/i){
+            answer += 2;
+        }else{
+            answer += 1;
+        }
+    }
+    cout<<answer<<"\n";
+    return 0;
+}
