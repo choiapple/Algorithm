deee329f9a24662d76cd7e29631bcc3e5e59af9a
최진석 <79374648+choiapple@users.noreply.github.com>
2025-04-03 14:44:01 +0900

    [Silver IV] Title: 청기 백기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N, answer = 0;
+    cin>>N;
+    for(int i=1; i*i<=N; i++){
+        answer++;
+    }
+    cout<<answer<<"\n";
+    return 0;
+}
