3068bf64878e9a29252b4e2e3fecb24da901609b
최진석 <79374648+choiapple@users.noreply.github.com>
2025-05-15 08:13:34 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long N;
+    cin>>N;
+    if(N%2 == 1){
+        cout<<"SK"<<"\n";
+    }else{
+        cout<<"CY"<<"\n";
+    }
+    return 0;
+}
