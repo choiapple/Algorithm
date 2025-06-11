a6313ec8ed35ef307ceed7c4422afab1190dd547
최진석 <79374648+choiapple@users.noreply.github.com>
2025-06-11 23:38:28 +0900

    [Bronze I] Title: SWAPC, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/32158. SWAPC/SWAPC.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int visit[101] = {0, };
+    int S;
+    string st;
+    char cp;
+    cin>>S;
+    cin>>st;
+    for(int i=0; i<S; i++){
+        if(visit[i] == 1) continue;
+        if(st[i] != 'C' && st[i] != 'P') continue;
+        for(int j=i+1; j<S; j++){
+            if(visit[j] == 1) continue;
+            if(st[j] != 'C' && st[j] != 'P') continue;
+            if(st[i] != st[j]){
+                cp = st[i];
+                st[i] = st[j];
+                st[j] = cp;
+                visit[i] = 1;
+                visit[j] = 1;
+                break;
+            }
+        }
+    }
+    cout<<st<<"\n";
+    return 0;
+}
