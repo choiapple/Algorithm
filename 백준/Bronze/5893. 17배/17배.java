6bb66c9b40bef79dd21fc83ee8ee796d464e76e6
최진석 <79374648+choiapple@users.noreply.github.com>
2025-07-13 11:27:22 +0900

    [Bronze III] Title: 17배, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/5893. 17배/17배.java
+++ 백준/Bronze/5893. 17배/17배.java
@@ -5,26 +5,13 @@ import java.math.BigInteger;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		BigInteger bi = BigInteger.ZERO;
-		BigInteger one = new BigInteger("1");
-		BigInteger two = new BigInteger("2");
-		String answer = "";
-		int len = s.length();
-		for(int i=len-1; i>=0; i--) {
-			bi = bi.add(one.multiply(new BigInteger(s.charAt(i) + "")));
-			one = one.multiply(two);
-		}
-		bi = bi.multiply(new BigInteger("17"));
-		while(bi.compareTo(BigInteger.ZERO) == 1) {
-			answer += bi.remainder(two);
-			bi = bi.divide(two);
-		}
-		for(int i=answer.length()-1; i>=0; i--) {
-			System.out.print(answer.charAt(i));
-		}
-	}
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String binaryInput = br.readLine();
+
+        BigInteger number = new BigInteger(binaryInput, 2); // 이진수 → 십진수
+        BigInteger result = number.multiply(BigInteger.valueOf(17)); // 17배
+        System.out.println(result.toString(2)); // 십진수 → 이진수 출력
+    }
 
 }
