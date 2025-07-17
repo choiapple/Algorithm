a4c7f19dfa52da9632bb8b8ffb01e95dbd58f8e8
최진석 <79374648+choiapple@users.noreply.github.com>
2025-07-17 12:39:56 +0900

    [Bronze III] Title: Ahmed Aly, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10698. Ahmed Aly/Ahmed Aly.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T, x, y, z;
+    char pm, e;
+    cin>>T;
+    for(int t=1; t<=T; t++){
+        cin>>x>>pm>>y>>e>>z;
+        cout<<"Case "<<t<<": ";
+        if(pm == '+'){
+            if(x + y == z){
+                cout<<"YES"<<"\n";
+            }else{
+                cout<<"NO"<<"\n";
+            }
+        }else{
+            if(x - y == z){
+                cout<<"YES"<<"\n";
+            }else{
+                cout<<"NO"<<"\n";
+            }
+        }
+    }
+}
