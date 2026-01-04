49bb71c300000574166d0c7c0c226a6ceac7f05c
최진석 <79374648+choiapple@users.noreply.github.com>
2026-01-04 13:34:43 +0900

    [Gold IV] Title: 가르침, Time: 172 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -3,15 +3,9 @@
 using namespace std;
 int N, K, answer = 0;
 vector<string> vc;
-string full = "bdefghjklmopqrsuvwxyz";
 int arr[26] = {0, };
-void re(int x, string find, int j){
-    if(j > 21) return;
-    if(x == K){
-        for(int i=0; i<find.length(); i++){
-            int y = find[i] - 'a';
-            arr[y] = 1;
-        }
+void re(int x, int j){
+    if(x >= K){
         int tmp = 0;
         for(int n=0; n<N; n++){
             int flag = 0;
@@ -25,14 +19,14 @@ void re(int x, string find, int j){
             if(flag == 0) tmp++;
         }
         if(tmp > answer) answer = tmp;
-        for(int i=0; i<find.length(); i++){
-            int y = find[i] - 'a';
-            arr[y] = 0;
-        }
         return;
     }
-    re(x + 1, find + full[j], j+1);
-    re(x, find, j+1);
+    for(int i = j; i<26; i++){
+        if(arr[i] == 1) continue;
+        arr[i] = 1;
+        re(x+1, i+1);
+        arr[i] = 0;
+    }
 }
 int main(){
     ios_base::sync_with_stdio(false);
@@ -43,7 +37,7 @@ int main(){
         cout<<0;
         return 0;
     }
-    string s = "antic", ss, sss = "";
+    string s = "antatica", ss;
     for(int i=0; i<s.length(); i++){
         int t = s[i] - 'a';
         arr[t] = 1;
@@ -52,7 +46,7 @@ int main(){
         cin>>ss;
         vc.push_back(ss);
     }
-    re(5, sss, 0);
+    re(5, 0);
     cout<<answer<<"\n";
     return 0;
 }
