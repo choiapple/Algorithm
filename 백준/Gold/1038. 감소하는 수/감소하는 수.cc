88a31c347a72911c803585e90ffd7d10b13f2c47
최진석 <79374648+choiapple@users.noreply.github.com>
2026-01-11 15:23:36 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.cc
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -2,7 +2,7 @@
 #include <queue>
 using namespace std;
 int N;
-long long DP[10000010];
+long long answer = 0;
 int main(){
     cin>>N;
     if(0 <= N && N <= 10){
@@ -12,7 +12,6 @@ int main(){
     queue<long long> q;
     for(int i=1; i<=9; i++){
         q.push(i);
-        DP[i] = i;
     }
     int idx = 10;
     while(idx <= N && q.empty() == false){
@@ -21,10 +20,11 @@ int main(){
         int last = num % 10;
         for(int i=0; i<last; i++){
             q.push(num*10 + i);
-            DP[idx++] = num * 10 + i;
+            if(idx == N) answer = num*10 + i;
+            idx++;
         }
     }
-    if(idx >= N && DP[N] != 0) cout<<DP[N]<<"\n";
+    if(idx >= N && answer != 0) cout<<answer<<"\n";
     else cout<<-1<<"\n";
     return 0;
 }
