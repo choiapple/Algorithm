12977928a567c46c0c8dc3b2b795739272699fbb
최진석 <79374648+choiapple@users.noreply.github.com>
2026-01-22 22:52:05 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, K;
+    cin>>N;
+    for(int n=0; n<N; n++){
+        cin>>K;
+        cout<<1<<"\n";
+    }
+    return 0;
+}
