3ba19952207169a9e6f06fbe995b6f83aba58239
최진석 <79374648+choiapple@users.noreply.github.com>
2026-01-31 01:39:54 +0900

    [Bronze II] Title: 내 스트릭을 돌려내!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30395. 내 스트릭을 돌려내！/내 스트릭을 돌려내！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, arr[1001] = {0, }, max = 0, cur = 0, f = -2;
+    cin>>N;
+    for(int n=0; n<N; n++){
+        cin>>arr[n];
+    }
+    for(int n=0; n<N; n++){
+        if(arr[n] > 0) cur++;
+        else if(n - f >= 2) f = n;
+        else cur = 0;
+        if(max < cur) max = cur;
+    }
+    cout<<max;
+    return 0;
+}
